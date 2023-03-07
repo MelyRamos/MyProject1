3165a874cecbeca7194482b2f854ba00ddb6fb0d
Mely Ramos <100741860+MelyRamos@users.noreply.github.com>
2023-03-07 12:54:57 -0600

    Update HiWord.java

--- src/der/HiWord.java
+++ src/der/HiWord.java
@@ -4,6 +4,29 @@ import java.util.Scanner;
 public class HiWord {
 	public static void main(String[] args) {
 		System.out.println("Hello Word");
+		//calcular la media aritmetica
+			  Scanner sc = new Scanner(System.in);
+		      int n, sum = 0, x;
+
+		      float media;
+
+		      System.out.print("Ingrese la cantidad de números: ");
+
+		      n = sc.nextInt();
+
+		      System.out.println("Ingrese los números: ");
+
+		      for (int i = 0; i < n; i++) {
+
+		         x = sc.nextInt();
+
+		         sum += x;
+
+		      }
+
+		      media = (float) sum / n;
+
+		      System.out.println("La media aritmética es: " + media);
 
    }
 
